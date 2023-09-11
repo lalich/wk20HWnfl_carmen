7291e80f7253e97d79a757b0a58431d33bd5794f
Mark D. Lalich <mdlalich@gmail.com>
2023-09-11 14:22:15 -0700

    The finishing touches

--- findcarmen.sql
+++ findcarmen.sql
@@ -3,7 +3,7 @@
 -- use two `-` to comment at the start of a line, or highlight the text and press `⌘/` to toggle comments
 -- EXAMPLE: SELECT ALL FROM THE TABLE COUNTRY AND LIMIT IT TO ONE ENTRY
 
-SELECT * FROM COUNTRY LIMIT 1;
+-- SELECT * FROM COUNTRY LIMIT 1;
 
 --  -[ RECORD 1 ]--+--------------------------
 -- code           | AFG
@@ -23,33 +23,85 @@ SELECT * FROM COUNTRY LIMIT 1;
 -- code2          | AF
 
 
--- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
+
+-- Clue #1: We recently got word that someone fitting Carmen Sandiego's description 
+-- has been traveling through Southern Europe. 
+-- She's most likely traveling someplace where she won't be noticed, 
+-- so find the least populated country in Southern Europe, 
+-- and we'll start looking for her there.
+SELECT name FROM country
+WHERE region = 'Southern Europe'
+ORDER BY population ASC
+
+-- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes 
+-- in this country's officially recognized language. Check our databases and find out what language is spoken in this country, 
+-- so we can call in a translator to work with you.
+
+SELECT c.name AS country_name, cl.language as official_language
+FROM country c
+JOIN countrylanguage cl ON c.code = cl.countrycode
+WHERE c.region = 'Southern Europe' AND cl.isofficial = TRUE
+
+-- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country,
+-- a country where people speak only the language she was learning.
+-- Find out which nearby country speaks nothing but that language.
+
+SELECT c.name AS country_name, cl.language as official_language, cl.percentage
+FROM country c
+JOIN countrylanguage cl ON c.code = cl.countrycode
+WHERE c.region = 'Southern Europe' AND cl.isofficial = TRUE
+ORDER BY cl.percentage DESC
+-- SMR, ITALIAN
+
+-- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. 
+-- There are only two cities she could be flying to in the country. One is named the same as the country 
+-- that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
+-- (SMR)
+
+SELECT name 
+FROM city
+WHERE countrycode = 'SMR' AND name != 'San Marino'
+
+-- Serravalle
 
 
+-- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, 
+-- but in totally different parts of the globe! She's headed to South America as we speak; 
+-- go find a city whose name is like the one we were headed to, but doesn't end the same. 
+-- Find out the city, and do another search for what country it's in. Hurry!
+-- Serravalle
 
--- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
+SELECT c.name AS country_name, cl.language as official_language, cl.percentage, code
+FROM country c
+JOIN countrylanguage cl ON c.code = cl.countrycode
+WHERE c.region = 'South America' AND language = 'Italian'
+
+SELECT name, code FROM country
+WHERE region = 'South America'
+ORDER BY population ASC
+
+SELECT name 
+FROM city
+WHERE countrycode = 'ARG' OR countrycode = 'BRA', name ILIKE 'Serravile%'
+
+SELECT name, countrycode
+FROM city
+WHERE (countrycode = 'ARG' OR countrycode = 'BRA') AND name LIKE '%Ser%'
+-- Argentina or Brazil
+-- ARG or BRA
+-- 'Serra, Brazil'
 
 
+-- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, 
+-- and is headed towards the capital! Look up the country's capital, and get there pronto! 
+-- Send us the name of where you're headed and we'll follow right behind you!
 
+SELECT city.name
+FROM city
+JOIN country ON city.id = country.capital
+WHERE country.code = 'BRA'
 
--- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
-
-
-
-
--- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
-
-
-
-
--- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
-
-
-
--- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
-
-
+-- 'Brasilia'
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
@@ -65,8 +117,9 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
-
-
-
+SELECT name
+FROM city
+WHERE population = '91084'
 
 -- She's in ____________________________!
+SANTA MONICA!!!!
